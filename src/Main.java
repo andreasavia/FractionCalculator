3b95f7523dbdf4c7349ae35a767120a1b75f61e9
Andrea Savia <asavia3@gmail.com>
2020-06-27 14:13:04 +0100

    Files added with structure

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    public String getOperation(Scanner input) {
+        return "placeholder";
+
+    }
+    public boolean validFraction(String input) {
+        return false;
+    }
+    /* public Fraction getFraction(Scanner input) {
+
+    } */
+
+}
