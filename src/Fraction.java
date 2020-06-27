f66a5b83dd2a492227500c8d237a45ab7f8d3b66
Andrea Savia <asavia3@gmail.com>
2020-06-27 18:04:56 +0100

    Completed Fraction object methods

--- src/Fraction.java
+++ src/Fraction.java
@@ -27,38 +27,58 @@ public class Fraction {
     public Fraction() {
         this(0,1);
     }
+    
     public int getNumerator() {
         return num;
     }
     public int getDenominator() {
         return den;
-    } /*
+    } 
     public String toString() {
-
+        return this.num + "/" + this.den;
     }
     public double toDouble() {
-
-    }   
+        return this.num / this.den;
+    }
     public Fraction add(Fraction other) {
-
+        int added_den = this.den * other.den;
+        int added_num = (this.num * other.den) + (other.num * this.den);
+        Fraction added = new Fraction(added_num,added_den);
+        added.toLowestTerms();;
+        return added;
     }
     public Fraction subtract(Fraction other) {
-
+        int sub_den = this.den * other.den;
+        int sub_num = (this.num * other.den) - (other.num * this.den);
+        Fraction sub = new Fraction(sub_num,sub_den);
+        sub.toLowestTerms();;
+        return sub;
     }
     public Fraction multiply(Fraction other) {
-
+        Fraction prod = new Fraction(this.num * other.num, this.den * other.den);
+        return prod;
     }
     public Fraction divide(Fraction other) {
-
+        Fraction div = new Fraction(this.num * other.den, this.den * other.num);
+        return div;
     }
     public boolean equals(Object other) {
-
+        Fraction new_other = new Fraction();    
+        new_other = (Fraction) other;
+        return (this.toDouble() == new_other.toDouble());
     }
     public void toLowestTerms() {
-
+        int fract_gcd = gcd(this.num, this.den);
+        this.num = this.num / fract_gcd;
+        this.den = this.den / fract_gcd;
     }
-    public int gcd(int num, int den) {
-
+    public int gcd(int num, int den) { // find greatest common denominator
+        int rem;
+        while (num != 0 && den !=0) {
+            rem = num % den;
+            num = den;
+            den = rem;
+        }
+        return num;
     }
-    */
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        
+        Fraction a = new Fraction(4,8);
+        Fraction b = new Fraction(2,8);
+        Fraction sum = a.add(b);
+        System.out.println(sum.toString());
+
     }
     public String getOperation(Scanner input) {
         return "placeholder";
@@ -15,5 +19,4 @@ public class Main {
     /* public Fraction getFraction(Scanner input) {
 
     } */
-
 }
